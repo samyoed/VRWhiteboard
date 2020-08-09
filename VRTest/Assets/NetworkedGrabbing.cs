da89b31060dd078e3f2e197a28354fe04ffd4254
samyoed <shl638@nyu.edu>
2020-08-09 19:39:33 -0400

    Started google drive integration again, more work on networking, was able to sort of network the markers

---
+++ VRTest/Assets/NetworkedGrabbing.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbacks
+{
+    PhotonView photonView;
+
+    void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnSelectEnter()
+    {
+        TransferOwnership();
+    }
+    public void OnSelectExit()
+    {
+
+    }
+    void TransferOwnership()
+    {
+        TransferOwnership();
+    }
+
+    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
+    {
+        print("OnOwnership Requested for: " + targetView.name + " from " + requestingPlayer.NickName);
+        photonView.TransferOwnership(requestingPlayer);
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+    {
+        print("Transfer is complete. New owner: " + targetView.Owner.NickName);
+    }
+
+}
--- VRTest/Assets/NotLocalDisable.cs
+++ VRTest/Assets/NotLocalDisable.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class NotLocalDisable : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class NotLocalDisable : MonoBehaviour
     public GameObject leftHand;
     public GameObject rightHand;
     public GameObject cam;
+    public GameObject ground;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,9 @@ public class NotLocalDisable : MonoBehaviour
                 rightHand.SetActive(false);
                 cam.SetActive(false);
             }
+
+        ground = GameObject.Find("Ground");
+        //ground.GetComponent<TeleportationArea>().TeleportationProvider = GetComponent<;
     }
 
     // Update is called once per frame
